dc8c1bb65c729aaf7cc21426a2f08d0530fc06d6
Максим Андрейчук <maximandreychuk1999@gmail.com>
2024-08-19 13:09:19 +0300

    file + bytes

---
+++ mod_7_2.py
@@ -0,0 +1,20 @@
+def custom_write(file_name: str, strings: list) -> dict[tuple, str]:
+
+    strings_positions ={}
+    with open(file_name, 'w', encoding='utf-8') as f:
+        for index, string in enumerate(strings):
+            position = f.tell()
+            f.write(string + 'n')
+            strings_positions[(index + 1, position)] = string
+
+    return strings_positions
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+print(custom_write("1", strings=info))
